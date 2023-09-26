3c75330f6f36bfc94bc81a682ca6bd34e353a14c
edvardsanta <eduardosantamarco123@outlook.com>
2023-09-26 11:55:59 -0300

    added binary and octal conversion from decimal
    - hexadecimal was just assigning the values not summing.
    - more tests 💪 (`▿´) 👊

--- src/decimal.zig
+++ src/decimal.zig
@@ -1,18 +1,22 @@
-// Examples
+// Imports
 const std = @import("std");
 const expect = std.testing.expect;
 const math = @import("std").math;
-const BASE_HEXA = 16;
-pub const Error = error{EverythingIsBroken};
 const stdout = std.io.getStdOut().writer();
-pub fn decimalHexadecimal(decimal: i32) Error!i32 {
-    var remainder = [_]u8{};
-    _ = remainder;
+const bufPrint = std.fmt.bufPrint;
+
+// Const values
+const BASE_HEXA = 16;
+const BASE_OCTAL = 8;
+const BASE_BINARY = 2;
+pub const Error = error{ EverythingIsBroken, NoSpaceLeft };
+
+pub fn decimalHexadecimal(decimal: i64) Error!i64 {
     var quotient = decimal;
 
     var count: i32 = 0;
 
-    var validate_hexa: i32 = 0x0;
+    var validate_hexa: i64 = 0x0;
     while (quotient != 0) {
         const scoped_remainder = @rem(quotient, BASE_HEXA);
         const base = math.pow(i32, BASE_HEXA, @as(i32, count));
@@ -22,16 +26,16 @@ pub fn decimalHexadecimal(decimal: i32) Error!i32 {
                     validate_hexa += 0xA * base;
                 },
                 11 => {
-                    validate_hexa = 0xB * base;
+                    validate_hexa += 0xB * base;
                 },
                 12 => {
-                    validate_hexa = 0xC * base;
+                    validate_hexa += 0xC * base;
                 },
                 13 => {
-                    validate_hexa = 0xD * base;
+                    validate_hexa += 0xD * base;
                 },
                 14 => {
-                    validate_hexa = 0xE * base;
+                    validate_hexa += 0xE * base;
                 },
                 15 => {
                     validate_hexa += 0xF * base;
@@ -39,8 +43,7 @@ pub fn decimalHexadecimal(decimal: i32) Error!i32 {
                 else => {},
             }
         } else {
-            const y = @as(i32, scoped_remainder);
-            validate_hexa += y * base;
+            validate_hexa += scoped_remainder * base;
         }
         quotient = @divTrunc(quotient, BASE_HEXA);
         count += 1;
@@ -54,20 +57,149 @@ pub fn decimalHexadecimal(decimal: i32) Error!i32 {
 
     return validate_hexa;
 }
-pub fn decimalOctal() u16 {
-    return 0o755;
+pub fn decimalOctal(decimal: i64) Error!i64 {
+    var quotient = decimal;
+
+    var count: i32 = 0;
+
+    var validate_octal: i64 = 0o0;
+    while (quotient != 0) {
+        const scoped_remainder = @rem(quotient, BASE_OCTAL);
+        const base = math.pow(i32, BASE_OCTAL, @as(i32, count));
+
+        if (scoped_remainder > 8) {
+            validate_octal += scoped_remainder - 2 * base;
+        } else {
+            validate_octal += scoped_remainder * base;
+        }
+        quotient = @divTrunc(quotient, BASE_OCTAL);
+        count += 1;
+    }
+    if (validate_octal == decimal) {
+        stdout.print("Octal value {o}\n", .{validate_octal}) catch {};
+    } else {
+        stdout.print("everything is broken ˙◠˙", .{}) catch {};
+        return error.EverythingIsBroken;
+    }
+    return validate_octal;
 }
 
-pub fn decimalBinary() u8 {
-    return 0b1111_0000;
+pub fn decimalBinary(decimal: i64) Error!i64 {
+    var quotient = decimal;
+
+    var count: i32 = 0;
+
+    var validate_binary: i64 = 0b0;
+    while (quotient != 0) {
+        const scoped_remainder = @rem(quotient, BASE_BINARY);
+        const base = math.pow(i32, BASE_BINARY, @as(i32, count));
+
+        validate_binary += scoped_remainder * base;
+
+        quotient = @divTrunc(quotient, BASE_BINARY);
+        count += 1;
+    }
+    if (validate_binary == decimal) {
+        stdout.print("Binary value {b}\n", .{validate_binary}) catch {};
+    } else {
+        stdout.print("everything is broken ˙◠˙", .{}) catch {};
+        return error.EverythingIsBroken;
+    }
+    return validate_binary;
 }
 
-test "check hexadecimal conversion, expected value must be the same of input" {
+test "check hexadecimal conversioni with random nunmbe, expected value must be the same of input" {
     // arrange
-    const expected_value = 800;
+    const RndGen = std.rand.DefaultPrng;
+    var rnd = RndGen.init(0);
+    var some_random_num = rnd.random().int(i32);
+    try stdout.print("Value generated: {d} ", .{some_random_num});
+    //act
+    var value = decimalHexadecimal(some_random_num) catch |err| {
+        // assert error
+        try expect(err == error.EverythingIsBroken);
+        return;
+    };
+
+    // assert
+    try expect(value == some_random_num);
+}
+
+test "check hexadecimal conversion, expected value must be the same of input (specific)" {
+    // arrange
+    const expected_value = 452;
+    try stdout.print("Specific value: {d} ", .{expected_value});
 
     //act
-    var value = decimalHexadecimal(800) catch |err| {
+    var value = decimalHexadecimal(452) catch |err| {
+        // assert error
+        try expect(err == error.EverythingIsBroken);
+        return;
+    };
+
+    // assert
+    try expect(value == expected_value);
+}
+
+test "check octal conversion, expected value must be the same of input" {
+    // arrange
+    const RndGen = std.rand.DefaultPrng;
+    var rnd = RndGen.init(0);
+    var some_random_num = rnd.random().int(i32);
+    try stdout.print("Value generated: {d} ", .{some_random_num});
+
+    //act
+    var value = decimalOctal(some_random_num) catch |err| {
+        // assert error
+        try expect(err == error.EverythingIsBroken);
+        return;
+    };
+
+    // assert
+    try expect(value == some_random_num);
+}
+
+test "check octal conversion, expected value must be the same of input (specific)" {
+    // arrange
+    const expected_value = 297;
+    try stdout.print("Specific value: {d} ", .{expected_value});
+
+    //act
+    var value = decimalOctal(297) catch |err| {
+        // assert error
+        try expect(err == error.EverythingIsBroken);
+        return;
+    };
+
+    // assert
+    try expect(value == expected_value);
+}
+
+test "check binary conversion, expected value must be the same of input" {
+    // arrange
+    const RndGen = std.rand.DefaultPrng;
+    var rnd = RndGen.init(0);
+    var some_random_num = rnd.random().int(i32);
+    try stdout.print("Value generated: {d} ", .{some_random_num});
+
+    //act
+    var value = decimalBinary(some_random_num) catch |err| {
+        // assert error
+        try expect(err == error.EverythingIsBroken);
+        return;
+    };
+
+    // assert
+    try expect(value == some_random_num);
+}
+
+test "check binary conversion, expected value must be the same of input (specific)" {
+    // arrange
+    const expected_value = 582;
+    try stdout.print("Specific value: {d} ", .{expected_value});
+
+    //act
+    var value = decimalBinary(582) catch |err| {
         // assert error
         try expect(err == error.EverythingIsBroken);
         return;
--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,28 @@
 const std = @import("std");
 const decimal = @import("decimal.zig");
-
+const expect = std.testing.expect;
+const eql = std.mem.eql;
+const bufPrint = std.fmt.bufPrint;
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     while (true) {
-        try stdout.print("Letś convert to hexadecimal numbers.\n", .{});
+        try stdout.print("Letś convert to hexadecimal, octal and binary numbers.\n", .{});
 
         const guess = try ask_user();
         const dont_fail_please = try decimal.decimalHexadecimal(guess);
         _ = dont_fail_please;
 
+        const dont_fail_please_octal = try decimal.decimalOctal(guess);
+        _ = dont_fail_please_octal;
+
+        const dont_fail_please_bit = try decimal.decimalBinary(guess);
+        _ = dont_fail_please_bit;
+
         try stdout.print("Here we are again.\n", .{});
     }
 }
 
-fn ask_user() !i32 {
+fn ask_user() !i64 {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
 
@@ -23,7 +31,7 @@ fn ask_user() !i32 {
     try stdout.print("Write a number: ", .{});
 
     if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
-        return std.fmt.parseInt(i32, user_input, 10);
+        return std.fmt.parseInt(i64, user_input, 10);
     } else {
         return error.InvalidParam;
     }
